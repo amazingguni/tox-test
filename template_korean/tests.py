cdd638995fe0a782921b7d81bde0f0886ab8447f
amazingguni <amazingguni@gmail.com>
2016-08-15 17:23:23 +0900

    a

--- template_korean/tests.py
+++ template_korean/tests.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 import korean
 from django.test import TestCase
 
@@ -13,11 +14,11 @@ class TemplateKoreanTestCase(TestCase):
         self.assertEquals(1, 1)
 
     def test_korean_proofread(self):
-        result = korean.l10n.proofread("나(은)는 사람이다.")
-        self.assertEqual("나는 사람이다.", result)
+        result = korean.l10n.proofread(u"나(은)는 사람이다.")
+        self.assertEqual(u"나는 사람이다.", result)
 
     def test_korean_proofread_fail(self):
-        result = korean.l10n.proofread("나(은)는 사람이다.")
+        result = korean.l10n.proofread(u"나(은)는 사람이다.")
         self.assertNotEqual(u"나은 사람이다.", result)
 
     def test_korean_proofread_Tag(self):
@@ -26,6 +27,6 @@ class TemplateKoreanTestCase(TestCase):
             '{% load proofread %}'
             '{{ string|proofread }}'
         ).render(Context({
-            'string': '나(은)는 밥(을)를 먹었다.',
+            'string': u'나(은)는 밥(을)를 먹었다.',
         }))
-        self.assertEqual(rendered, '나는 밥을 먹었다.')
+        self.assertEqual(rendered, u'나는 밥을 먹었다.')
